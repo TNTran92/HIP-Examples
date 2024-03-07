23f692cf3df86232b27b903c804e3a87d8525f6a
TNTran92 <tran.aggies@gmail.com>
2024-03-06 20:13:34 -0600

    Simplest working HIP Compiling Example

---
+++ openmp-helloworld/build/CMakeFiles/test_openmp_helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_openmp_helloworld.dir/openmp_helloworld.cpp.o"
+  "CMakeFiles/test_openmp_helloworld.dir/openmp_helloworld.cpp.o.d"
+  "test_openmp_helloworld"
+  "test_openmp_helloworld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_openmp_helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
