c4356a1f4b5e32efe5d006c29f75a7973ce0c4d4
TNTran92 <tran.aggies@gmail.com>
2024-03-09 09:37:50 -0600

    add obsolete

--- clang/obsolete/kernels.cuh
+++ clang/obsolete/kernels.cuh
@@ -1,11 +1,10 @@
-#include "hip/hip_runtime.h"
 // Copyright (c) Facebook, Inc. and its affiliates.
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
 #include <float.h>
-#include <ops.hip.h>
+#include <ops.cuh>
 
 #ifndef kernels
 #define kernels
