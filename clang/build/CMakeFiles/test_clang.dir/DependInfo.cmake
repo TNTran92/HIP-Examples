25a995785aa14e7b79cfc8b7d06eb3d459a91bd2
TNTran92 <tran.aggies@gmail.com>
2024-03-12 21:57:40 -0500

    save

--- clang/build/CMakeFiles/test_clang.dir/DependInfo.cmake
+++ clang/build/CMakeFiles/test_clang.dir/DependInfo.cmake
@@ -8,11 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/rocm-user/project/HIP_example/HIP-Examples/clang/common.hip.cpp" "CMakeFiles/test_clang.dir/common.hip.cpp.o" "gcc" "CMakeFiles/test_clang.dir/common.hip.cpp.o.d"
   "/home/rocm-user/project/HIP_example/HIP-Examples/clang/cpu_ops.hip.cpp" "CMakeFiles/test_clang.dir/cpu_ops.hip.cpp.o" "gcc" "CMakeFiles/test_clang.dir/cpu_ops.hip.cpp.o.d"
-  "/home/rocm-user/project/HIP_example/HIP-Examples/clang/kernels.hip.cpp" "CMakeFiles/test_clang.dir/kernels.hip.cpp.o" "gcc" "CMakeFiles/test_clang.dir/kernels.hip.cpp.o.d"
-  "/home/rocm-user/project/HIP_example/HIP-Examples/clang/ops.hip.cpp" "CMakeFiles/test_clang.dir/ops.hip.cpp.o" "gcc" "CMakeFiles/test_clang.dir/ops.hip.cpp.o.d"
-  "/home/rocm-user/project/HIP_example/HIP-Examples/clang/pythonInterface.hip.cpp" "CMakeFiles/test_clang.dir/pythonInterface.hip.cpp.o" "gcc" "CMakeFiles/test_clang.dir/pythonInterface.hip.cpp.o.d"
   )
 
 # Targets to which this target links.
